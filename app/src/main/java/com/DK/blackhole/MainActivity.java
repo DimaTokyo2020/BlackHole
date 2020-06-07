6cdb92e38e82781892d2f4689ab34d426c369be3
E016729 <Dmitry.Kroytor@elbitsystems.com>
2020-06-07 10:15:39 +0300

    Add opening camera app from intent and capture the new image

--- app/src/main/java/com/DK/blackhole/MainActivity.java
+++ app/src/main/java/com/DK/blackhole/MainActivity.java
@@ -1,15 +1,31 @@
 package com.DK.blackhole;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,14 +41,56 @@ import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 import jcifs.smb.SmbFileOutputStream;
 
-public class MainActivity extends AppCompatActivity {
+import static android.view.View.*;
+import static androidx.core.graphics.TypefaceCompatUtil.getTempFile;
 
+/**
+ * https://abhiandroid.com/programming/camera
+ * */
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    TextView text,text1;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    //Bitmap photo;
+    String photo;
+    private SQLiteDatabase db;
+    Bitmap theImage;
+    Activity activity;
+    ImageView newPhotoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verifyStoragePermissions(this);
+        activity = this;
+        newPhotoImageView = findViewById(R.id.newPhotoImageView);
+
+
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.i("dima", "request");
+            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+        }
+
+        Button cameraButton = findViewById(R.id.takeImageButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+
+                    Log.i("dima", "intent");
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+
+        });
+
+
+
+
     }
 
 
@@ -204,5 +262,40 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            theImage = (Bitmap) data.getExtras().get("data");
+            newPhotoImageView.setImageBitmap(theImage);
+
+            //photo=getEncodedString(theImage);
+            //setDataToDataBase();
+        }
+
+
+    }
+    private String getEncodedString(Bitmap bitmap){
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, os);
+
+  /* or use below if you want 32 bit images
+
+   bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);*/
+
+        byte[] imageArr = os.toByteArray();
+
+        return Base64.encodeToString(imageArr, Base64.URL_SAFE);
+
+    }
+
+
+
 }
+
+
 
